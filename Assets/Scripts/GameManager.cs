8b0164dceb82f198a460f37dc96662deec6435cb
FrittoMisto <43555658+samuelemir@users.noreply.github.com>
2022-04-15 19:13:58 +0200

    main menu
    - added title and credits
    - added a start button
    - added a GameManager to persist in both scenes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private int menuScene = 0;
+    private int gameScene = 1;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+
+    public void LoadMenuScene()
+    {
+        SceneManager.LoadScene(menuScene);
+    }
+}
